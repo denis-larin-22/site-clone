350d73133153171e1d194292b270c9ebfce7e38b
Denis <denislarin2017@gmail.com>
2025-07-31 13:53:01 +0300

    Check

--- app/catalog/[categoryId]/category/page.tsx
+++ app/catalog/[categoryId]/category/page.tsx
@@ -20,13 +20,13 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
         };
     };
 
-    const imageUrl = getCategoryImagePath(params.categoryId);
+    const imageUrl = getCategoryImagePath(activeCategory.id);
 
     return {
         title: activeCategory.name,
         description: `Товари з категорії ${activeCategory.name}` || `Товари з каталогу Piramid`,
         openGraph: {
-            title: '🛒' + activeCategory.name + '⬇️',
+            title: '🛍️ ' + activeCategory.name + ' ⬇️',
             description: "Ознайомтесь з найкращими пропозиціями цієї категорії і обирайте те, що створене саме для вас. 🌟",
             url: `https://piramidspace.com/catalog/${params.categoryId}/category`,
             type: 'website',
@@ -45,16 +45,18 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     };
 }
 
-function getCategoryImagePath(category: string) {
+function getCategoryImagePath(category: number) {
     const path = 'https://piramidspace.com/assets/images/'
 
     switch (category) {
-        case '1': return path + 'day-night.webp'
-        case '2': return path + 'roller-blinds.webp'
-        case '3': return path + 'horizontal-blinds.webp'
-        case '4': return path + 'vertical-blinds.webp'
-        case '5': return path + 'components.webp'
-        case '6': return path + 'promotional-items.webp'
+        case 102: return path + 'top.webp'
+        case 101: return path + 'sale.webp'
+        case 1: return path + 'day-night.webp'
+        case 2: return path + 'roller-blinds.webp'
+        case 3: return path + 'horizontal-blinds.webp'
+        case 4: return path + 'vertical-blinds.webp'
+        case 5: return path + 'components.webp'
+        case 6: return path + 'promotional-items.webp'
         default: return path + 'default-item.webp'
     }
 }
