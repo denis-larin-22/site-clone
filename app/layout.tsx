231422fc0eaf85c02c1b42e49a71a3c3f380ea1f
Denis <denislarin2017@gmail.com>
2025-09-24 11:04:26 +0300

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,11 @@ export default function RootLayout({
     <html lang="uk">
       <head>
         {/* Google Analytics */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KL986K9TLM" strategy="afterInteractive" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KL986K9TLM"
+          strategy="afterInteractive"
+        />
         <Script
           id="google-analytics"
           strategy="afterInteractive"
@@ -37,17 +41,27 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-NT6VWX4K');`}
         </Script>
 
-
+        {/* Binotel виджет */}
+        <Script id="binotel-widget" strategy="afterInteractive">
+          {`(function(d, w, s) {
+            var widgetHash = '3oohp5gsdv47j5n53mm0', gcw = d.createElement(s); 
+            gcw.type = 'text/javascript'; gcw.async = true;
+            gcw.src = '//widgets.binotel.com/getcall/widgets/' + widgetHash + '.js';
+            var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
+          })(document, window, 'script');`}
+        </Script>
       </head>
 
-      <body className={`${comfortaaFont.className} bg-t-pale relative min-h-screen flex flex-col justify-between`}>
+      <body
+        className={`${comfortaaFont.className} bg-t-pale relative min-h-screen flex flex-col justify-between`}
+      >
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-NT6VWX4K"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
